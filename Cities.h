c61695f09d6ec9d5a37df4dd2e4de116eb6a64eb
OGHaruki <66510475+OGHaruki@users.noreply.github.com>
2023-04-29 23:48:50 +0200

    Update Cities.h

--- Cities.h
+++ Cities.h
@@ -3,17 +3,19 @@
 #include "String.h"
 #include "List.h"
 #include "Vector.h"
+#include "HashMap.h"
+#include "PriorityQueue.h"
 using namespace std;
 
 class Cities {
 private:
     int width = 0, height = 0;
-    Vector<Vector<char>> map;
+    Vector<char> map;
     struct City {
         String name;
         int id;
-        int coordinates_x;
         int coordinates_y;
+        int coordinates_x;
         struct Connection {
             City* city;
             int distance;
@@ -22,23 +24,22 @@ private:
         City() {
             this->name = "";
             this->id = 0;
-            this->coordinates_x = 0;
             this->coordinates_y = 0;
+            this->coordinates_x = 0;
         }
         explicit City(String name) {
             this->name = name;
         }
     };
     Vector<City> cities;
+    HashMap citiesMap;
 public:
     Cities();
     Cities(const int width, const int height);
     ~Cities();
     void ReadInput(String& source, String& destination, int &distance);   // funkcja wczytujaca dane
     void InsertMap();   // funkcja wczytujaca mape miast
-    void InsertFlights();   // funkcja wczytujaca loty
-    void PrintMap();    // funkcja wypisujaca mape miast
-    void PrintCities(); // funkcja wypisujaca miasta
+    void InsertFlights(char* source, char* destination, char* distance);   // funkcja wczytujaca loty
     void InsertCities();    // funkcja wczytujaca nazwy miast
     void BFS();             // funkcja wyznaczajaca polaczenia dla kazdego miasta
     void CalculateShortestWay(String& source, const String& destination, int mode);    // funkcja wyznacza najszybszą trasę z A do B
