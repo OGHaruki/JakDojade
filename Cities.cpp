6d4d9b9698b8d2f1879c583448041c5acb5bd568
OGHaruki <66510475+OGHaruki@users.noreply.github.com>
2023-04-29 23:46:49 +0200

    Update Cities.cpp

--- Cities.cpp
+++ Cities.cpp
@@ -19,28 +19,46 @@ void Cities::BFS() {
     bool found = false;
     bool *visited = new bool[height * width];
     int *distance = new int[height * width];
+    City **coordinates = new City*[height * width];
+    int temp = 0;
     for (int i = 0; i < height * width; i++) {
         visited[i] = false;
         distance[i] = 0;
+        if(map[i] == '*') {
+            coordinates[i] = &cities[temp];
+            temp++;
+        }
+        else
+            coordinates[i] = nullptr;
     }
     for (int k = 0; k < cities.GetSize(); k++) {
-        DoubleLinkedList<std::pair<int, int>> queue;             // wykorzystuje wczesniej stworzona liste jako kolejke
-        queue.PushBack(std::make_pair(cities[k].coordinates_x, cities[k].coordinates_y));
+        int num_of_roads = 0;
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                if((i == -1 && j == 0) || (i == 0 && j == -1) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
+                    if(cities[k].coordinates_y + i >= 0 && cities[k].coordinates_y + i < height && cities[k].coordinates_x + j >= 0 && cities[k].coordinates_x + j < width) {
+                        if(map[(cities[k].coordinates_y + i) * width + cities[k].coordinates_x + j] == '#' ||
+                            map[(cities[k].coordinates_y + i) * width + cities[k].coordinates_x + j] == '*')
+                            num_of_roads++;
+                    }
+                }
+            }
+        }
+        if(num_of_roads == 0) {
+            continue;
+        }
+        DoubleLinkedList<std::pair<int, int>> queue;      // wykorzystuje wczesniej stworzona liste jako kolejke
+        queue.PushBack(std::make_pair(cities[k].coordinates_y, cities[k].coordinates_x));
         while (queue.GetSize() != 0) {
             std::pair<int, int> temp = queue[0];
             queue.RemoveByIndex(0);
             visited[temp.first * width + temp.second] = true;
-            if (map[temp.first][temp.second] == '*' && pom != 0) {
+            if (map[temp.first * width + temp.second] == '*' && pom != 0) {
                 found = true;
                 City::Connection c;
                 c.distance = distance[temp.first * width + temp.second];
-                for(int z = 0; z < cities.GetSize(); z++) {
-                    if (cities[z].coordinates_x == temp.first && cities[z].coordinates_y == temp.second) {
-                        c.city = &(cities[z]);
-                        cities[k].connections.PushBack(c);
-                        break;
-                    }
-                }
+                c.city = coordinates[temp.first * width + temp.second];
+                cities[k].connections.PushBack(c);
             }
             if(!found) {
                 for (int i = -1; i <= 1; i++) {
@@ -48,7 +66,7 @@ void Cities::BFS() {
                         if ((i == -1 && j == 0) || (i == 0 && j == -1) || (i == 0 && j == 1) || (i == 1 && j == 0)) {
                             if(temp.first + i >= 0 && temp.first + i < height && temp.second + j >= 0 && temp.second + j < width) {
                                 if(!visited[(temp.first + i) * width + (temp.second + j)]){
-                                    if(map[temp.first + i][temp.second + j] == '#' || map[temp.first + i][temp.second + j] == '*') {
+                                    if(map[(temp.first + i) * width + temp.second + j] == '#' || map[(temp.first + i) * width + temp.second + j] == '*') {
                                         queue.PushBack(std::make_pair(temp.first + i, temp.second + j));
                                         visited[(temp.first + i) * width + (temp.second + j)] = true;
                                         distance[(temp.first + i) * width + (temp.second + j)] = distance[temp.first * width + temp.second] + 1;
@@ -70,17 +88,14 @@ void Cities::BFS() {
     }
     delete[] visited;
     delete[] distance;
+    delete[] coordinates;
 }
 
 void Cities::InsertMap() {
     char c;
-    for(int i = 0; i < height; i++) {
-        Vector<char> temp;
-        for(int j = 0; j < width; j++) {
-            cin >> c;
-            temp.PushBack(c);
-        }
-        map.PushBack(temp);
+    for(int i = 0; i < height * width; i++) {
+        cin >> c;
+        map.PushBack(c);
     }
 }
 
@@ -91,28 +106,29 @@ void Cities::InsertCities() {
     bool flag = false;
     for(int i = 0; i < height; i++) {
         for(int j = 0; j < width; j++) {
-            if(map[i][j] == '*') {
+            if(map[i * width + j] == '*') {
                 for(int k = -1; k <= 1; k++) {
                     for(int n = -1; n <= 1; n++) {
                         if(k == 0 && n == 0) continue;
                         else {
                             if(i + k >= 0 && i + k < height && j + n >= 0 && j + n < width) {
-                                if (char(map[i + k][j + n]) >= 'A' && char(map[i + k][j + n]) <= 'Z'
-                                    || (char(map[i + k][j + n]) >= '0' && char(map[i + k][j + n]) <= '9')) {
+                                if (char(map[(i + k) * width + j + n]) >= 'A' && char(map[(i + k) * width + j + n]) <= 'Z'
+                                    || (char(map[(i + k) * width + j + n]) >= '0' && char(map[(i + k) * width + j + n]) <= '9')) {
                                     temp = n;
-                                    while(map[i + k][j + temp - 1] != '.' && map[i + k][j + temp - 1] != '#' && map[i + k][j + temp - 1] != '*' && j + temp - 1 >= 0) {
+                                    while(map[(i + k) * width + j + temp - 1] != '.' && map[(i + k) * width + j + temp - 1] != '#' && map[(i + k) * width + j + temp - 1] != '*' && j + temp - 1 >= 0) {
                                         temp--;
                                     }
-                                    while(map[i + k][j + temp] != '.' && map[i + k][j + temp] != '#' && map[i + k][j + temp] != '*' && j + temp < width) {
-                                        temp_name.PushBack(map[i + k][j + temp]);
+                                    while(map[(i + k) * width + j + temp] != '.' && map[(i + k) * width + j + temp] != '#' && map[(i + k) * width + j + temp] != '*' && j + temp < width) {
+                                        temp_name.PushBack(map[(i + k) * width + j + temp]);
                                         temp++;
                                     }
                                     flag = true;
                                     City c(temp_name);
                                     c.id = index;
-                                    c.coordinates_x = i;
-                                    c.coordinates_y = j;
+                                    c.coordinates_y = i;
+                                    c.coordinates_x = j;
                                     cities.PushBack(c);
+                                    citiesMap.SetItem(temp_name, index);
                                     break;
                                 }
                             }
@@ -130,57 +146,44 @@ void Cities::InsertCities() {
 }
 
 void Cities::CalculateShortestWay(String& source, const String& destination, int mode) {
-    int final_index = 0;
-    int start_index = 0;
-    int *distance = new int[cities.GetSize()];
-    City** previous = new City*[cities.GetSize()];
-    DoubleLinkedList<City*> queue;
-    for(int i = 0; i < cities.GetSize(); i++) {
-        if(cities[i].name == destination) {
-            final_index = i;
-        }
-        if(cities[i].name == source) {
-            start_index = i;
-        }
-        distance[i] = INF;
-        previous[i] = nullptr;
-        queue.PushBack(&(cities[i]));
-    }
+    int final_index = citiesMap.GetItem(destination);
+    int start_index = citiesMap.GetItem(source);
     if(start_index == final_index) {
         cout << "0" << endl;
         return;
     }
+    bool *isInQueue = new bool[height * width];
+    int *distance = new int[cities.GetSize()];
+    City** previous = new City*[cities.GetSize()];
     distance[start_index] = 0;
-    int temp_distance;
-    int remove_index;
+    previous[start_index] = nullptr;
+    PriorityQueue<City*> queue;
+    for(int i = 0; i < cities.GetSize(); i++) {
+        if(i != start_index) {
+            distance[i] = INT_MAX;
+            previous[i] = nullptr;
+            isInQueue[i] = false;
+        }
+    }
+    queue.Push(&(cities[start_index]), distance[start_index]);
+    isInQueue[start_index] = true;
     City* current;
     while(queue.GetSize() != 0) {
         int total_distance = 0;
-        temp_distance = INF;
-        for(int j = 0; j < queue.GetSize(); j++) {
-            if(distance[queue[j]->id] <= temp_distance) {
-                temp_distance = distance[queue[j]->id];
-                current = queue[j];
-                remove_index = j;
-            }
-        }
-        queue.RemoveByIndex(remove_index);          // usuwamy z kolejki miasto o najmniejszej odl
-        if (current->id == final_index) {
-            break;
-        }
+        current = queue.Pop();
+        isInQueue[current->id] = false;
         for(int j = 0; j < current->connections.GetSize(); j++) {
-            for(int k = 0; k < queue.GetSize(); k++) {
-                int queueID = queue[k]->id;
-                int id = current->connections[j].city->id;
-                if (queueID == id) {
-                    total_distance = distance[current->id] + cities[current->id].connections[j].distance;
-                    if (total_distance < distance[current->connections[j].city->id]) {
-                        distance[current->connections[j].city->id] = total_distance;
-                        previous[current->connections[j].city->id] = &cities[current->id];
-                    }
+            total_distance = distance[current->id] + cities[current->id].connections[j].distance;
+            if (total_distance < distance[current->connections[j].city->id]) {
+                distance[current->connections[j].city->id] = total_distance;
+                previous[current->connections[j].city->id] = &cities[current->id];
+                if (!isInQueue[current->connections[j].city->id]) {
+                    queue.Push(&cities[current->connections[j].city->id], total_distance);
+                    isInQueue[current->connections[j].city->id] = true;
                 }
             }
         }
+        //if(current->id == final_index) break;
     }
     if(mode == 0) {
         cout << distance[final_index];
@@ -189,17 +192,11 @@ void Cities::CalculateShortestWay(String& source, const String& destination, int
         cout << distance[final_index] << " ";
         Vector<String> ShortestPath;
         City *temp;
-        // koncowe miasto znajduje sie pod indeksem final_index
         temp = previous[final_index];
         if(previous[final_index] != nullptr) {
             while(temp != nullptr && !(temp->name == source)) {
                 ShortestPath.PushBack(temp->name);
-                for (int i = 0; i < cities.GetSize(); i++) {
-                    if (cities[i].name == temp->name) {
-                        final_index = i;
-                        break;
-                    }
-                }
+                final_index = citiesMap.GetItem(temp->name);
                 temp = previous[final_index];
             }
         }
@@ -207,17 +204,9 @@ void Cities::CalculateShortestWay(String& source, const String& destination, int
             cout << ShortestPath[i] << " ";
         }
     }
+    delete[] isInQueue;
     delete[] previous;
     delete[] distance;
-}
-
-void Cities::PrintMap() {
-    for(int i = 0; i < height; i++) {
-        for(int j = 0; j < width; j++) {
-            cout << map[i][j];
-        }
-        cout << endl;
-    }
 }
 
 Cities::~Cities() {
@@ -227,19 +216,10 @@ Cities::~Cities() {
     cities.Clear();
 }
 
-void Cities::PrintCities() {
-    for(int i = 0; i < cities.GetIndex(); i++) {
-        cout << "City: " << cities[i].name << endl;
-        for(int j = 0; j < cities[i].connections.GetIndex(); j++) {
-            cout << cities[i].connections[j].city->name << " - " << cities[i].connections[j].distance << endl;
-        }
-    }
-}
-
 int StringToInt(String& time) {
     int distance = 0;
     for(int i = 0; i < time.GetLength(); i++) {
-         distance = distance * 10 + (time[i] - '0');
+        distance = distance * 10 + (time[i] - '0');
     }
     return distance;
 }
@@ -268,26 +248,39 @@ void Cities::ReadInput(String& source, String& destination, int &distance) {
     distance = StringToInt(time);
 }
 
-void Cities::InsertFlights() {
-    String source, destination;
-    int distance = 0;
-    ReadInput(source, destination, distance);
+void Cities::InsertFlights(char* source, char* destination, char* distance) {
+    char c;
+    int size = 0;
+    do {
+        c = fgetc(stdin);
+        source[size] = c;
+        size++;
+    } while(c != ' ');
+    source[size - 1] = '\0';
+    size = 0;
+    do {
+        c = fgetc(stdin);
+        destination[size] = c;
+        size++;
+    } while(c != ' ');
+    destination[size - 1] = '\0';
+    size = 0;
+    do {
+        c = fgetc(stdin);
+        distance[size] = c;
+        size++;
+    } while(c != '\n');
+    distance[size - 1] = '\0';
     City::Connection new_connection;
-    new_connection.distance = distance;
-    for(int i = 0; i < cities.GetSize(); i++) {
-        if(cities[i].name == destination) {
-            new_connection.city = &cities[i];
-            break;
-        }
+    new_connection.distance = atoi(distance);
+    int index = citiesMap.GetItem(destination);
+    if(index != -1) {
+        new_connection.city = &cities[index];
     }
-    for(int i = 0; i < cities.GetSize(); i++) {
-        if(cities[i].name == source) {
-            cities[i].connections.PushBack(new_connection);
-            break;
-        }
+    index = citiesMap.GetItem(source);
+    if(index != -1) {
+        cities[index].connections.PushBack(new_connection);
     }
-    source.Clear();
-    destination.Clear();
 }
 
 void Cities::PrintShortestWay() {
